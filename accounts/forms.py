6e1248811bc64c36fa0470ab2f5da33583a3ed52
Silas <silasandrade.code@gmail.com>
2025-11-22 15:52:05 -0300

    Add custom user creation form
    Introduces UserCreateForm extending Django's UserCreationForm for the custom User model, including additional fields and translated labels.

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.utils.translation import gettext as _
+from django.forms import ModelForm
+from .models import User
+
+class UserCreateForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'fullname',
+            'phone_number',
+            'email',
+            'date_of_birth',
+            'password1',
+            'password2',
+        ]
+        labels = {
+            'fullname': _("Full name"),
+            'phone_number': _("Phone Number"),
+            'email': _("E-mail"),
+            'date_of_birth': _("Date of birth"),
+            'password1': _("Password"),
+            'password2': _("Repeat the password"),
+        }
