88e6bb6f7ec03ba9f14d1b186aec7e28afc5a370
Silas Andrade <silasandradee@outlook.com>
2026-02-10 14:17:50 -0300

    fix: Correções do Serializers de Review, ReviewMedia e ReiewReaction

--- apps/reviews/serializers.py
+++ apps/reviews/serializers.py
@@ -1,5 +1,3 @@
-import os
-
 from rest_framework import serializers
 
 from .models import (
@@ -7,45 +5,71 @@ from .models import (
     ReviewMedia,
     ReviewReaction
 )
-from core import settings
-
-class ReviewSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Review
-        fields = ['id', 'product', 'text']
+from .constants import MAX_REVIEW_IMAGE_SIZE, MAX_REVIEW_VIDEO_SIZE
+from apps.users.serializers import ReadUserSerializer
 
 
-    def create(self, validated_data):
-        return Review.objects.create(
-            user=self.context["request"].user,
-            **validated_data
-        )
 
-class ReviewMediaSerializer(serializers.ModelSerializer):
+class CreateReviewMediaSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReviewMedia
-        fields = ['id', 'review', 'file', 'media_type', 'order']
+        fields = ['file', 'media_type', 'order']
     
     def validate(self, attrs):
         file = attrs["file"]
         content_type = file.content_type
 
-        if content_type.startswith("image/") and file.size > settings.MAX_REVIEW_IMAGE_SIZE:
-            raise serializers.ValidationError("Imagem muito grande (máx 5MB).")
+        if content_type.startswith("image/") and file.size > MAX_REVIEW_IMAGE_SIZE:
+            raise serializers.ValidationError("Very large image (max 5MB).")
 
-        if content_type.startswith("video/") and file.size > settings.MAX_REVIEW_VIDEO_SIZE:
-            raise serializers.ValidationError("Vídeo muito grande (máx 30MB).")
+        if content_type.startswith("video/") and file.size > MAX_REVIEW_VIDEO_SIZE:
+            raise serializers.ValidationError("Very large video (max 30MB).")
 
         return attrs
 
 
-class ReviewReactionSerializer(serializers.ModelSerializer):
+class ReadReviewMediaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReviewMedia
+        fields = [
+            'id',
+            'review',
+            'file',
+            'media_type',
+            'order',
+        ]
+
+
+class CreateReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['text']
+
+    #TODO Validar se o produto está publicado ou não para publicação
+
+class ReadReviewSerializer(serializers.ModelSerializer):
+    user = ReadUserSerializer(read_only=True)
+    media = ReadReviewMediaSerializer(read_only=True, many=True)
+    class Meta:
+        model = Review
+        fields = [
+            'id',
+            'user',
+            'product', 
+            'text',
+            'media',
+            'created_at'
+            ]
+
+
+class UpdateReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['text']
+
+
+
+class CreateReviewReactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReviewReaction
         fields = ['id', 'review', 'is_helpful']
-    
-    def create(self, validated_data):
-        return Review.objects.create(
-            user=self.context["request"].user,
-            **validated_data
-        )
